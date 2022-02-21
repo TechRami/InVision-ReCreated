e609dc073a3c1c1500cd41bcd42091762cf2a671
Rami <sh3sh3c1@gmail.com>
2022-02-21 21:27:09 +0300

    just updated that the nav toggler will turn to black color on scroll

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    if ($(window).scrollTop() > $(window).height()) {
+    if ($(window).scrollTop() > 400) {
       $(".mynav").css({ "background-color": "#ffffff" });
       $(".nav-link").css("color", "black");
+      $(".togSVG").css("fill", "black");
     } else {
       $(".mynav").css({ "background-color": "transparent" });
       $(".nav-link").css("color", "white");
+      $(".togSVG").css("fill", "white");
     }
   });
 });
